c31f2aae6523f014b5838acf230f9daf93f31873
BederCC <beder1810@gmail.com>
2025-02-19 01:37:14 -0500

    map

---
+++ server/01-map.js
@@ -0,0 +1,14 @@
+const letters = ['a', 'b', 'c', 'd', 'e'];
+const numbers = [1, 2, 3, 4, 5];
+
+const newArray = letters.map(item => item + '++');
+
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+const newArray2 = numbers.map(item => item * 2);
+console.log(newArray2);
+
+console.log('original array:', letters);
+console.log('new array:', newArray);
